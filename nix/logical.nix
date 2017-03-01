3ad95d8e1071a412e80bd05b04e2b2927ae04c86
System administrator <root@ip-172-31-41-106.eu-west-1.compute.internal>
2017-03-01 13:45:30 +0000

    nixx add

---
+++ nix/logical.nix
@@ -0,0 +1,46 @@
+{
+  network.description = "NIXOPS server";
+
+  webserver =
+    { config, pkgs, ... }:
+    { 
+
+      services.httpd.enable = true;
+      services.httpd.adminAddr = "maxxl@tut.by";
+      services.httpd.documentRoot = "/www";
+      
+      services.logrotate.enable = true;
+      services.logrotate.config = ''
+        /var/log/temp/*.loo {
+            missingok
+            hourly
+            rotate 8
+            compress
+        }
+        '';
+
+      networking.firewall.allowedTCPPorts = [ 80 3000 22 ];
+
+services.grafana = {
+    enable=true;
+    port=3000;
+    addr = "0.0.0.0";
+};
+
+
+      services.postgresql.enable = true;
+      services.postgresql.authentication = "local all all ident";
+
+      environment.systemPackages = [
+          pkgs.jdk
+          pkgs.wget
+          pkgs.mc
+          pkgs.sqlite
+          pkgs.php
+          pkgs.logrotate
+          pkgs.git
+        ];
+
+    };
+
+}
