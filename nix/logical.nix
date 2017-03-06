ffe62dad886b1c796871b7b1d8a4a4e59137cdc2
maxxll <maxxl@tut.by>
2017-03-06 14:05:19 +0200

    Update logical.nix

--- nix/logical.nix
+++ nix/logical.nix
@@ -84,7 +84,7 @@
 services.grafana = {
     enable=true;
     port=3000;
-    addr = "0.0.0.0";
+    #addr = "0.0.0.0";
     database.host = "127.0.0.1:5432";
     database.name = "grafana";
     database.user = "grafana";
