7c7fb9915d6d8335198c3f87870eaacafb485967
Max <maxxl@tut.by>
2017-03-06 07:25:59 +0000

    comm

---
+++ nix/ec2.nix
@@ -0,0 +1,27 @@
+let
+
+  region = "eu-west-1c";
+  securityGroups = [ "secure_group" "default"];
+  accessKeyId = "dev"; # symbolic name looked up in ~/.ec2-keys
+#  networking.firewall.allowedTCPPorts = [21 80 2003 10051 3000 8080];
+
+
+  ec2 =
+    { resources, ... }:
+    { deployment.targetEnv = "ec2";
+      deployment.ec2.accessKeyId = accessKeyId;
+      deployment.ec2.region = region;
+      deployment.ec2.instanceType = "t1.micro";
+#      deployment.ec2.securityGroupIds = [ "secure_group" ];
+      deployment.ec2.keyPair = resources.ec2KeyPairs.my-key-pair;
+    };
+    
+in
+{ 
+
+  nixos = ec2;
+
+  # Provision an EC2 key pair.
+  resources.ec2KeyPairs.my-key-pair =
+    { inherit region accessKeyId; };
+}
