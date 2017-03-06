2fac4ef1eb99794681c84f80101f381f0c78e0d9
maxxll <maxxl@tut.by>
2017-03-06 14:02:02 +0200

    Update logical.nix

--- nix/logical.nix
+++ nix/logical.nix
@@ -17,6 +17,19 @@
      web.enable = true;
       carbon = {
        enableCache = true;
+       config = {
+       ''
+  [cache]
+  # Listen on localhost by default for security reasons
+  UDP_RECEIVER_INTERFACE = 127.0.0.1
+  PICKLE_RECEIVER_INTERFACE = 127.0.0.1
+  LINE_RECEIVER_INTERFACE = 127.0.0.1
+  CACHE_QUERY_INTERFACE = 127.0.0.1
+  # Do not log every update
+  LOG_UPDATES = False
+  LOG_CACHE_HITS = False  
+''
+};
       };
     };
 
