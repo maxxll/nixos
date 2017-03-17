ce8900140c5080ca6f2574d72df79f0642848fe4
maxxll <maxxl@tut.by>
2017-03-17 14:48:11 +0200

    Update grafana4.nix

--- pkgs/grafana4.nix
+++ pkgs/grafana4.nix
@@ -4,13 +4,4 @@
 #    path = [pkgs.grafana];
 #    wantedBy = [ "default.target" ];
 #        serviceConfig = {
-#        };    
-
-
-    systemd.services.grafana4 = {
-      description = "Grafana 4";
-      path = [ pkgs.grafana ];
-      serviceConfig = {
-        Type = "simple";        
-      };
-      };
+#        };
