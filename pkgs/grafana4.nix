4bb9920119b001cad67f800eb3545315efdb8df3
maxxll <maxxl@tut.by>
2017-03-17 14:13:30 +0200

    Update grafana4.nix

--- pkgs/grafana4.nix
+++ pkgs/grafana4.nix
@@ -1,7 +1,7 @@
 pkgs : {
     description = "Grafana 4";
     enable = true;
-    path = [pkgs."grafana-v4.1.2"];
+    path = [pkgs.grafana-v4];
     wantedBy = [ "default.target" ];
         serviceConfig = {
         };    
