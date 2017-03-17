33ee5e381c522df5a947a19c8c4f2a79ec95968e
maxxll <maxxl@tut.by>
2017-03-17 14:11:22 +0200

    Update configuration.nix

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -5,6 +5,7 @@
     swapDevices = [ { device = "/var/swapfile"; size = 8192; } ];
     
     systemd.services.sysstat  = import /root/nixos_rep/pkgs/sysstat.nix  pkgs;
+    systemd.services.grafana4  = import /root/nixos_rep/pkgs/grafana4.nix  pkgs;
         
     systemd.timers.vvv = {
     wantedBy = [ "timers.target"];
