da56ebcfe5adafff81a0201e20d753532759ab92
maxxll <maxxl@tut.by>
2017-03-20 16:21:03 +0300

    Update configuration.nix

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -52,12 +52,14 @@
    enable = true;
    addr = "0.0.0.0";
    port = 4000;
-   database.host = "127.0.0.1:5432";
-   database.name = "grafana";
-   database.password = "Post-1";
-   database.user = "grafana";
-   database.type = "postgresql";
-   database.path = "/var/lib/grafana/data/grafana.db";
+   
+   #database.host = "127.0.0.1:5432";
+   #database.name = "grafana";
+   #database.password = "Post-1";
+   #database.user = "grafana";
+   #database.type = "postgresql";
+   #database.path = "/var/lib/grafana/data/grafana.db";
+   
    security = {
      adminUser = "admin";
      adminPassword = "admin";
